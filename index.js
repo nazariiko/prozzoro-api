60535aa1e4c4f5bdb54d7d015ec3f9d5b19cd439
nazariiko <nazarii.dev@gmail.com>
2023-10-27 19:22:48 +0300

    fix

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const filtersRouter = require('./routes/filters');
 app.use('/', tenderRouter);
 app.use('/', filtersRouter);
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
