29461640eab6c40d77ebd80df7abb10f030ad3bf
nazariiko <nazarii.dev@gmail.com>
2023-10-28 13:19:33 +0300

    fix

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ require('dotenv').config();
 const tenderRouter = require('./routes/tender');
 const filtersRouter = require('./routes/filters');
 
+const cors = require("cors");
+app.use(cors({
+  origin: '*'
+}));
+
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
 app.use('/', tenderRouter);
 app.use('/', filtersRouter);
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 const port = process.env.PORT || 3000;
 
